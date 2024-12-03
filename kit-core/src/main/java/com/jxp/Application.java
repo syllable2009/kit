24bd5784b17629ccdebfe97f5036f95b773bc322
jiaxiaopeng <jiaxiaopeng@kuaishou.com>
2024-12-03 11:49:00 +0800

    update:save

--- kit-core/src/main/java/com/jxp/Application.java
+++ kit-core/src/main/java/com/jxp/Application.java
@@ -1,5 +1,7 @@
 package com.jxp;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -8,6 +10,11 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.google.common.base.Joiner;
+import com.google.common.collect.Lists;
+
+import cn.hutool.core.util.StrUtil;
+import lombok.extern.slf4j.Slf4j;
 import redis.clients.jedis.JedisPool;
 
 
@@ -15,10 +22,12 @@ import redis.clients.jedis.JedisPool;
  * Hello world!
  *
  */
+@Slf4j
 @RestController
 @SpringBootApplication
 public class Application {
     public static void main(String[] args) {
+//        log.info("{}", getLuaScript("a", Lists.newArrayList("2", "1", "3")));
         SpringApplication.run(Application.class, args);
     }
 
@@ -48,5 +57,23 @@ public class Application {
     public ResponseEntity<String> deleteValue(@RequestParam String key) {
         final Long del = jedisPool.getResource().del(key);
         return ResponseEntity.ok(Long.toString(del));
+    }
+
+    @GetMapping("/eval")
+    public ResponseEntity<List<String>> evalLuaScript(@RequestParam String key) {
+        final String luaScript = getLuaScript(key, Lists.newArrayList("2", "1", "3"));
+        // 此时脚本返回为null
+        jedisPool.getResource().eval(luaScript);
+//        int start = (pageNum - 1) * pageSize; // 从0开始
+//        int end = start + pageSize - 1;
+        final List<String> lrange = jedisPool.getResource().lrange(key, 0, -1);
+        return ResponseEntity.ok(lrange);
+    }
+
+    private static String getLuaScript(String key, List<String> value) {
+        String luaScript =
+                "redis.call('DEL', '{}') " +
+                        "redis.call('RPUSH', '{}', '{}')";
+        return StrUtil.format(luaScript, key, key, Joiner.on("','").skipNulls().join(value));
     }
 }
